c5a6bc56be3e7024d6f9565cae82a6e6f738fe85
VitaliiMakeev <vagontravki16@gmail.com>
2023-08-13 20:33:56 +0300

    добавил кол-во одной игрушки, изменил рандом и view

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
 package org.example;
 
 import org.example.moduls.NoFolder;
-import org.example.moduls.Toy;
 import org.example.view.View;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws NoFolder {
--- view/View.java
+++ view/View.java
@@ -23,33 +23,55 @@ public class View {
         System.out.println("Для прекращения загрузки введите 0 (ноль).");
         boolean flag = true;
         while (flag){
-            System.out.println("Введите название " + (toyList.size() + 1) + " игрушки и частоту выпадения: ");
-            String toy = scanner.nextLine();
+            System.out.println("Введите название " + (toyList.size() + 1) + " игрушки и частоту выпадения и количество:  ");
+            String tmp = scanner.nextLine();
+            String toy = tmp.trim();
             if (toy.trim().charAt(0) == '0'){
                 break;
             }
             List<String> strList = List.of(toy.split(" "));
             try{
+                if (strList.size() != 3){
+                    throw new RuntimeException("Неверный формат!(имя, вес, кол-во!)");
+                }
                 int n = Integer.parseInt(strList.get(1));
+                int n1 = Integer.parseInt(strList.get(2));
                 String name = strList.get(0).trim();
-                toyList.add(new Toy(shop, name, n));
+                toyList.add(new Toy(shop, name, n, n1));
             } catch (RuntimeException ex) {
-                System.out.println("Попробуйте еще раз!");
+                System.out.println(ex.getMessage());
             } catch (Exception e){
-                System.out.println("Необходимо ввести целое число!(1 - 9)");
+                System.out.println("Необходимо ввести целое число!(1 - 100)");
             }
 
         }
         Controller2 controller = new Controller2(toyList);
-        System.out.println("В таком порядке выпали бы игрушки 10 раз!");
-        System.out.println("id вес name");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(controller.get());
-        }
         SaveData saveData = new SaveData();
-        String res = saveData.saveData(controller.getToyQueue());
-        System.out.println(res);
-        System.out.println("Готово!");
+
+        while (flag){
+            System.out.println("Выдать игрушку?(y/n): ");
+            String vop = scanner.nextLine();
+            if (vop.length() == 0){
+                System.out.println("Необходимо ответить на вопрос!(y/n)");
+            }else if (vop.trim().charAt(0) != 'y' && vop.trim().charAt(0) != 'n'){
+                System.out.println("Необходимо ответить на вопрос!(y/n)");
+            } else if (vop.trim().charAt(0) == 'y') {
+                try{
+                    System.out.println("id вес имя кол-во");
+                    System.out.println(controller.get());
+                } catch (RuntimeException e1){
+                    System.out.println(e1.getMessage());
+                    String res = saveData.saveData(controller.getToyQueue());
+                    System.out.println(res);
+                    flag = false;
+                }
+            } else if (vop.trim().charAt(0) == 'n') {
+                String res = saveData.saveData(controller.getToyQueue());
+                System.out.println(res);
+                System.out.println("Готово!");
+                flag = false;
+            }
+        }
 
     }
 
